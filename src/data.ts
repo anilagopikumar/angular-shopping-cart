4ccb21ff5bcd22c9cbebd0688e5d6f5e8482a3c5
anilagopikumar <anila.desha094@gmail.com>
2020-10-07 07:35:42 +0530

    first commit

---
+++ src/data.ts
@@ -0,0 +1,45 @@
+import { CartItem } from './app/shopping-cart.service';
+export const DATA_ITEMS: CartItem[] = [
+  {
+    price: 59.99,
+    image: 'C:\Users\Anila\Downloads\angular-simple-rxjs-shopping-cart-example-ystcrr\src\1.jpg',
+    name: 'Fastrack Tropical Waters Green Dial Analog Watch for Women',
+    details: 'Dial Color: Black, Case Shape: Round,Band Color: Brown, Band Material: Leather,Watch Movement Type: Quartz, Watch Display Type: Analog,24 Months Manufacturer Warranty,Remove plastic at crown to start the watch',
+    heart: null,
+  },
+  {
+    price: 99.99,
+    image: 'C:\Users\Anila\Downloads\angular-simple-rxjs-shopping-cart-example-ystcrr\src\2.jpg',
+    name: 'Redux Analogue Black Dial Men’s & Boys Watch RWS0106S',
+    details: 'Dial Color: White, Case Shape: Round, Dial Glass Material: Mineral Band Color: Black, Band Material: Leather ; Lock Mechanism: Buckle ; Functionality: Plain 3-Hand ; Dial Type: European Numbers & Stick Watch Movement ; Type: Quartz, Watch Display Type: AnalogCase Material: Brass Water Resistance Depth: 30 meters Remove plastic at crown to start the watch Recommended for customers looking for a small dial sized watches',
+    heart: null,
+  },
+  {
+    price: 19.99,
+    image: 'C:\Users\Anila\Downloads\angular-simple-rxjs-shopping-cart-example-ystcrr\src\3.jpg',
+    name: 'Relish Analogue Black Dial Mens & Boys Watch',
+    details: 'Dial Color: White, Case Shape: Round, Dial Glass Material: Mineral Band Color: Black, Band Material: Leather ; Lock Mechanism: Buckle ; Functionality: Plain 3-Hand ; Dial Type: European Numbers & Stick Watch Movement ; Type: Quartz, Watch Display Type: AnalogCase Material: Brass Water Resistance Depth: 30 meters Remove plastic at crown to start the watch Recommended for customers looking for a small dial sized watches',
+    heart: null,
+  },
+  {
+    price: 59.99,
+    image: 'C:\Users\Anila\Downloads\angular-simple-rxjs-shopping-cart-example-ystcrr\src\4.jpg',
+    name: 'OVERFLY MEGALITH Luxury Chronograph Watch for Mens - NOW IN INDIA(6381)',
+    details: 'Dial Color: Black, Case Shape: Round,Band Color: Brown, Band Material: Leather,Watch Movement Type: Quartz, Watch Display Type: Analog,24 Months Manufacturer Warranty,Remove plastic at crown to start the watch',
+    heart: null,
+  },
+  {
+    price: 39.99,
+    image: 'C:\Users\Anila\Downloads\angular-simple-rxjs-shopping-cart-example-ystcrr\src\5.jpg',
+    name: 'SKMEI Analogue - Digital White Dial Mens Watch',
+    details : 'Dial Color: White, Case Shape: Round,Band Color: Brown, Band Material: Leather,Watch Movement Type: Quartz, Watch Display Type: Analog,Warranty type: Manufacturer; 2 Years Manufacturer Warranty',
+    heart: null,
+  },
+  {
+    price: 9.99,
+    image: 'C:\Users\Anila\Downloads\angular-simple-rxjs-shopping-cart-example-ystcrr\src\6.jpg',
+    name: 'CLOUDWOOD Barbie Kids Color Childrens Wrist Watch for Boys and Girls -W231',
+    details: 'Dial Color: Multi-Color; Item Shape: Round , Band Material: Rubber, Band color: Dark Pink ,Watch movement type: Quartz, Watch display type: Analog, Suitable For-Kids/ Boys/ Girl`s/ Childrens,Works on Japanese Quartz format',
+    heart: null,
+  }
+];
